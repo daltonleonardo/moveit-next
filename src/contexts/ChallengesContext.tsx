52eacd33ec477640fb328a55c8ecceea4ac451bd
dalton.leonardo <daltonleonardo@hotmail.com>
2021-02-27 08:47:53 -0300

    feature:  Create Cookie

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,8 +1,13 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+
 
 interface ChallengesProviderProps {
     children: ReactNode;
+    level: number
+    currentExperience: number 
+    challengesCompleted: number;
 }
 
 interface Challenge {
@@ -26,10 +31,13 @@ interface ChallengesContextData {
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children } : ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ 
+    children, 
+    ...rest
+    } : ChallengesProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
 
@@ -39,6 +47,14 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
     useEffect(() => {
         Notification.requestPermission();
     }, [])
+
+
+    //Vai disparar um atualização sempre que tiver uma atualização nos objetos abaixo.
+    useEffect(() => {
+        Cookies.set('level',String(level));
+        Cookies.set('currentExperience',String(currentExperience));
+        Cookies.set('challengesCompleted',String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted])
 
     function levelUp() {
       setLevel(level + 1);
